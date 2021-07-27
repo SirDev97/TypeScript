f1c5f7a3500cbf526aba5b71aaba74a4e3ca8e01
Alkin Maystorov <alkin971@gmail.com>
2021-07-27 14:07:28 +0200

    compile sanbox.ts to js

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,29 @@
-var character = "luigi";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+// Arrays
+var names = ["luigi", "mario", "yoshi"];
+// names = "test";
+names.push("toad");
+// names.push(3);
+// names[0] = true;
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push("shaggy");
+// numbers[1] = "fred";
+var mixed = ["ken", 5, "barbie", 10, true];
+mixed.push("faker");
+mixed.push(10);
+mixed[0] = 3;
+// Objects
+var ninja = {
+    name: "mario",
+    belt: "black",
+    age: 24
+};
+ninja.age = 40;
+ninja.name = "caps";
+// ninja.age = "30";
+// ninja.skills = ["tai-jutsu", "gen-jutsu"];
+ninja = {
+    name: "yoshi",
+    belt: "purple",
+    age: 40
+};
